5073d89e79bd5d53dcdfcf2d8dada7a326a0d15e
Kevin <kevinkwon1995@gmail.com>
2019-11-12 21:18:53 -0800

    Make api call to NASA and retrieve image to display. Added stub buttons

--- src/ImageContainer.js
+++ src/ImageContainer.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import styled from 'styled-components'
 import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button'
 
 const ImageContainerWrapper = styled(Container)`
   display: flex;
   justify-content: center;
-  border: 2px black solid;
-  height: 500px;
+  flex-direction: row;
+  height:100%;
+  margin-bottom: 100px;
 `
 
-function ImageContainer() {
-  return (
-    <ImageContainerWrapper maxWidth="md">
-      <p>Image</p>
-    </ImageContainerWrapper>
-  )
+class ImageContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUrl: null
+    }
+  }
+
+  // TODO: Temporary solution to hide API Key in github until backend service is ready
+  componentDidMount() {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+        this.setState({imageUrl: result.url})
+      })
+  }
+
+  render() {
+    return (
+      <ImageContainerWrapper maxWidth="lg">
+        <Button>previous button</Button>
+        <img src={this.state.imageUrl}/>
+        <Button>forward button</Button>
+      </ImageContainerWrapper>
+    )
+  }
 }
 
 export default ImageContainer
