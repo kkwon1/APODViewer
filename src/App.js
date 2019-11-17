d774bd87a1fbcb06555db60658b2cacc9d1eccd7
Kevin <kevinkwon1995@gmail.com>
2019-11-16 22:17:16 -0800

    Added date logic to pull correct data from NASA api

--- src/App.js
+++ src/App.js
@@ -2,18 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import ImageContainer from './ImageContainer'
 import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
 import NavBar from './NavBar'
 
 const MainContainer = styled.div`
   display: flex;
-`
-
-const HeaderContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  margin: 50px;
 `
 
 function App() {
@@ -22,13 +14,6 @@ function App() {
       <Grid container>
         <Grid item xs={12}>
           <NavBar/>
-        </Grid>
-        <Grid item xs={12}>
-          <HeaderContainer maxWidth="md">
-            <Typography variant="h2">
-              Welcome to APOD Viewer!
-            </Typography>
-          </HeaderContainer>
         </Grid>
         <Grid item xs={12}>
           <ImageContainer/>
--- src/ImageContainer.js
+++ src/ImageContainer.js
@@ -4,63 +4,124 @@ import Container from '@material-ui/core/Container'
 import IconButton from '@material-ui/core/IconButton'
 import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded'
 import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded'
+import moment from 'moment'
+import Typography from '@material-ui/core/Typography'
 
 const MainContainer = styled(Container)`
   display: flex;
   justify-content: space-around;
   flex-direction: row;
-  margin-bottom: 100px;
+  flex-direction: column;
+`
+
+const ImageSection = styled.div`
+  display:flex;
+  height: 800px;
 `
 
 const ImageContainerWrapper = styled(Container)`
   display: flex;
   justify-content: center;
   flex-direction: row;
+  margin-top: 50px;
+  margin-bottom: 100px;
 `
 
 const ButtonContainer = styled.div`
-  cursor: pointer;
   display: flex;
   align-items: center;
 `
 
+const TextContainer = styled.div`
+  display:flex;
+  align-items:center;
+  margin-bottom: 100px;
+`
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin-top: 50px;
+`
+
+const baseUrl = "https://api.nasa.gov/planetary/apod"
+const apiKey = process.env.REACT_APP_NASA_API_KEY
+const dateFormat = "YYYY-MM-DD"
+
+
+// TODO: Refactor this huge component into smaller chunks. Pass data down as props from parent
 class ImageContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageUrl: null
+      imageUrl: null,
+      imageDateIsToday: false,
+      currentImageDate: moment(),
+      text: "",
+      title: ""
     }
   }
 
   // TODO: Temporary solution to hide API Key in github until backend service is ready
   componentDidMount() {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-        this.setState({imageUrl: result.url})
-      })
+    this.getImageData(this.state.currentImageDate)
+  }
+
+  updateImageDate(event, numDaysToAdd) {
+    this.setState({currentImageDate: this.state.currentImageDate.add(numDaysToAdd, "days")})
+    this.getImageData(this.state.currentImageDate)
+  }
+
+  getImageData(date) {
+    fetch(`${baseUrl}?api_key=${apiKey}&date=${date.format(dateFormat)}`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+      this.setState({imageUrl: result.url})
+      this.setState({imageDateIsToday: dateIsToday(result.date)})
+      this.setState({text: result.explanation})
+      this.setState({title: result.title})
+    })
   }
 
   render() {
     return (
       <MainContainer>
-        <ButtonContainer>
-          <IconButton>
-            <ArrowBackIosRoundedIcon fontSize='large'/>
-          </IconButton>
-        </ButtonContainer>
-        <ImageContainerWrapper maxWidth="lg">
-          <img src={this.state.imageUrl}/>
-        </ImageContainerWrapper>
-        <ButtonContainer>
-          <IconButton>
-            <ArrowForwardIosRoundedIcon fontSize='large'/>
-          </IconButton>
-        </ButtonContainer>
+        <HeaderContainer maxWidth="md">
+          <Typography variant="h2">
+            {this.state.title}
+          </Typography>
+        </HeaderContainer>
+        <ImageSection>
+          <ButtonContainer>
+            <IconButton onClick={e => this.updateImageDate(e, -1)}>
+              <ArrowBackIosRoundedIcon fontSize='large'/>
+            </IconButton>
+          </ButtonContainer>
+          <ImageContainerWrapper maxWidth="lg">
+            <img src={this.state.imageUrl}/>
+          </ImageContainerWrapper>
+          <ButtonContainer>
+            <IconButton disabled={this.state.imageDateIsToday} onClick={e => this.updateImageDate(e, 1)}>
+              <ArrowForwardIosRoundedIcon fontSize='large'/>
+            </IconButton>
+          </ButtonContainer>
+        </ImageSection>
+        <TextContainer>
+          <Typography variant="body1">
+            {this.state.text}
+          </Typography>
+        </TextContainer>
       </MainContainer>
     )
   }
+}
+
+function dateIsToday(apodDate) {
+  let now = moment()
+  // apodDate is today or future date
+  return now <= moment(apodDate)
 }
 
 export default ImageContainer
