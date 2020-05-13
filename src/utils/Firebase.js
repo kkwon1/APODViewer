e21e9578cdc8efaed8b9e95eb4951b45642473d2
Kevin Kwon <kevinkwon1995@gmail.com>
2020-05-13 10:42:14 -0700

    Adding config file with dev env firebase

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,19 +1,8 @@
 import * as firebase from "../../node_modules/firebase/app";
 import "../../node_modules/firebase/auth";
+import firebase_config from "../data/firebase_config";
 
-const firebaseApiKey = process.env.REACT_APP_FIREBASE_API_KEY;
-
-const firebaseConfig = {
-  apiKey: firebaseApiKey,
-  authDomain: "apodviewer-2e111.firebaseapp.com",
-  databaseURL: "https://apodviewer-2e111.firebaseio.com",
-  projectId: "apodviewer-2e111",
-  storageBucket: "apodviewer-2e111.appspot.com",
-  messagingSenderId: "888665527053",
-  appId: "1:888665527053:web:508fdc7fbf64f530801644",
-  measurementId: "G-11EPTCFFN1",
-};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebase_config);
 
 export default firebase;
